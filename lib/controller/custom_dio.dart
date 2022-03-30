0e6953e6a74dc027f3ae4b55a071a12d1ffc1143
majid-d-d <75568204+majid-d-d@users.noreply.github.com>
2022-03-30 22:55:49 +0430

    مرحله اول درست کردن ایمپورت ها

--- lib/controller/custom_dio.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dio/dio.dart';
-import 'package:shared_value/shared_value.dart';
-import 'package:takfood_seller/view/pages/login_pages/splash_page.dart';
-
-class CustomDio {
-
-  static Dio dio = Dio(BaseOptions(baseUrl: 'https://kaghazsoti.uage.ir/api/', headers: headers),);
-}
